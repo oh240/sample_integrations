5fdce9feeb455831878e50353009b1862c190700
Nishio Takuya <littlecub240@gmail.com>
2014-09-24 00:08:31 +0900

    BDDプラグインの追加

--- app/Config/bootstrap/environments/ci.php
+++ app/Config/bootstrap/environments/ci.php
@@ -6,4 +6,6 @@ Environment::configure('ci',true,[
 	'MYSQL_DB_NAME' => 'test_blog',
 	'MYSQL_TEST_DB_TEST' => 'test_blog',
 	'MYSQL_PREFIX' => '',
-]);
+],function(){
+	CakePlugin::load('Bdd');
+});
